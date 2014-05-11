d6927104a2de67529ae490f81c71e1fbce70b273
glin <glin0728@163.com>
2014-05-11 15:29:01 +0800

    Initial Commit

---
+++ DemoQuartz/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DemoQuartz
+//
+//  Created by TopCoder on 5/11/14.
+//  Copyright (c) 2014 TCer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DemoQuartz/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DemoQuartz
+//
+//  Created by TopCoder on 5/11/14.
+//  Copyright (c) 2014 TCer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
